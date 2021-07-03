7e3a02427afb05d47ef4504cd593a2b877b3f170
Grant Wasil <dev@grantwasil.com>
2021-07-02 21:35:23 -0600

    feat: added Ably server

--- public/script.js
+++ public/script.js
@@ -15,6 +15,19 @@ let game;
 const BASE_SERVER_URL = "http://localhost:5000";
 const myNickname = localStorage.getItem("nickname");
 
+const realtime = Ably.Realtime({
+  authUrl: BASE_SERVER_URL + "/auth",
+});
+
+realtime.connection.once("connected", () => {
+  myClientId = realtime.auth.clientId;
+  gameRoom = realtime.channels.get("game-room");
+  deadPlayerCh = relatime.channels.get("dead-player");
+  myChannel = realtime.channels.get("clientChannel-" + myClientId);
+  gameRoom.presence.enter(myNickname);
+  game = new Phaser.Game(config);
+})
+
 class GameScene extends Phaser.Scene {
   constructor() {
     super("gameScene");
